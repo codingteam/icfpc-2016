0c071c50401011d50e29b414a1df4f3962515315
Ilya Portnov <portnov@iportnov.ru>
2016-08-05 16:36:03 +0500

    Flipping utilities.

--- src/Transform.hs
+++ src/Transform.hs
@@ -42,3 +42,25 @@ fromUnitSquare (TranslationData dx dy scale) silhouette = silhouette'
   where
   silhouette' = map (map $ (translate dx) *** (translate dy)) silhouette
   translate delta = (\x -> x - delta) . (* scale)
+
+-- | Flip point relative to segment
+flipPoint :: Segment -> Point -> Point
+flipPoint ((sx1,sy1), (sx2,sy2)) (x,y) =
+  if sx1 == sx2
+    then (2*sx1 - x, y)
+    else if sy1 == sy2
+           then (x, 2*sy1 - y)
+           else let k = (sy2-sy1) / (sx2-sx1)
+                    k' = -(sx2-sx1) / (sy2-sy1)
+                    b = sy1 - k * sx1
+                    b' = y - k' * x
+                    x0 = - (b - b') / (k - k')
+                    y0 = (b'*k - b*k') / (k - k')
+                    x' = 2*x0 - x
+                    y' = 2*y0 - y
+                in  (x', y')
+
+-- | Flip polygon relative to segment
+flipPolygon :: Segment -> Polygon -> Polygon
+flipPolygon seg points = map (flipPoint seg) $ reverse points
+
