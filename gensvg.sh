a968d93aec30c81763f195ffc69a502678433dd1
Ilya Portnov <portnov@iportnov.ru>
2016-08-06 15:39:57 +0500

    Update SVG

--- gensvg.sh
+++ gensvg.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd src
-stack build
 for input in ../problems/*.txt; do
     filename=`basename "$input"`
-    stack exec visualize -- "$input" -o "../svg/${filename%%txt}svg" -w 500
+    ./visualize -- "$input" -o "../svg/${filename%%txt}svg" -w 500
 done
