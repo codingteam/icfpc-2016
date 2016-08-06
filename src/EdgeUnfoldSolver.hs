65ea66601e39c0edaa79f1a0c75825520acc5e91
Alexander Batischev <eual.jp@gmail.com>
2016-08-06 13:13:02 +0300

    Implement `findFacets`

--- src/EdgeUnfoldSolver.hs
+++ src/EdgeUnfoldSolver.hs
@@ -1,6 +1,11 @@
 module EdgeUnfoldSolver where
 
+import Control.Arrow
 import Data.List
+import qualified Data.HashMap.Strict as M
+import qualified Data.HashSet as S
+
+import Debug.Trace
 
 import Problem
 
@@ -27,3 +32,27 @@ findUnfoldCandidates vertices segments =
     let ((x0, y0), (x1, y1)) = segment
     let (x2, y2) = vertex
     (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0)
+
+-- | Split @skeleton@ in two parts, one of which will be on the left, another
+-- on the right of the given @segment@ when we unfold the @skeleton@.
+--
+-- The parts aren't complete, closed skeletons! You should immediately merge
+-- them with @mergeSkeletons@.
+findFacets :: Skeleton -> Segment -> [Skeleton]
+findFacets skeleton (startPt, endPt) = go graph startPt []
+  where
+  graph1 = M.fromListWith S.union $ map (\(k, v) -> (k, S.singleton v)) skeleton
+  graph2 = M.fromListWith S.union $ map (\(v, k) -> (k, S.singleton v)) skeleton
+  graph = M.unionWith S.union graph1 graph2
+
+  go edges start facet
+    | M.null edges = [reverse facet]
+    | start == endPt =
+        -- avoid returning the segment we were given
+        if length facet > 1 then [reverse facet] else []
+    | otherwise =
+        let next = M.lookupDefault S.empty start edges
+            edges' = M.delete start edges
+
+            facets = map (\end -> (end, (start, end):facet)) (S.toList next)
+        in  concatMap (\(start', facet') -> go edges' start' facet') facets
