79c4f9323da36eec2485a83ddc044203958f3a41
Friedrich von Never <friedrich@fornever.me>
2016-08-06 18:08:04 +0700

    generator: prepare to cut with segments

--- src/generator.hs
+++ src/generator.hs
@@ -1,8 +1,11 @@
 
 module Main where
 
+import Data.Ratio
+
 import Printer
 import Problem
+import Solver
 
 fieldSize :: Number
 fieldSize = 1 / 1
@@ -25,6 +28,17 @@ initialProblem = Problem {
   pSkeleton = squareSkeleton
 }
 
+initialSegment :: Segment
+initialSegment = ((0, 0), ((1 % 2), (1 % 2)))
+
+foldProblem :: Segment -> Problem -> Problem
+foldProblem seg problem =
+  let silhouette = pSilhouette problem
+      skeleton = pSkeleton problem
+  in Problem {
+    pSilhouette = silhouette,
+    pSkeleton = skeleton
+  }
+
 main :: IO ()
-main = let text = serializeProblem initialProblem
-       in putStrLn text
+main = putStrLn $ serializeProblem $ foldProblem initialSegment initialProblem
