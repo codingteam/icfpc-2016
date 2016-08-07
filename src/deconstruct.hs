46f3958325fdf21c300e6c412c9f5f92d19d7c8a
Alexander Batischev <eual.jp@gmail.com>
2016-08-07 23:46:16 +0300

    deconstruct: show neighbouring edges

--- src/deconstruct.hs
+++ src/deconstruct.hs
@@ -2,10 +2,13 @@
 module Main where
 
 import Control.Monad
+import Data.List
 import Data.Ratio
 import Diagrams.Backend.SVG
 import Diagrams.Prelude
 import System.Environment
+
+import qualified Data.HashSet as S
 
 import Problem
 import Draw
@@ -16,7 +19,7 @@ main = do
   [path] <- getArgs
   problem <- parseProblem path
 
-  let points = concat $ map (\(p1, p2) -> [p1, p2]) $ pSkeleton problem
+  let points = nub $ concat $ map (\(p1, p2) -> [p1, p2]) $ pSkeleton problem
 
   let drawing = (# lc red) $ foldr1 atop $ map drawSegment $ pSkeleton problem
 
@@ -32,9 +35,38 @@ main = do
           === (text (show $ snd point) # fontSize 100 # lc black <> trect)
           ) # moveTo (mkPoint (1%2, (-1)%10))
 
+    let interesting =
+          ((# lc yellow) $
+          foldr1 atop $ map drawSegment (connected (pSkeleton problem) point))
+
     renderSVG
       ("point_" ++ show i ++ ".svg")
       (mkWidth 2000)
-      ((coordinates === (segmentsD `atop` drawing)) # bg white # bgFrame 0.1 white)
+      ((coordinates
+        ===
+        (segmentsD `atop` interesting `atop` drawing)) #
+      bg white #
+      bgFrame 0.1 white)
 
   print "That's all, folks!"
+
+-- | Returns all segments that are connected to a given point by another segment.
+connected :: Skeleton -> Problem.Point -> [Problem.Segment]
+connected skel pt =
+  filter
+    (\(p1, p2) -> ((p1 `S.member` ends || p2 `S.member` ends)
+               &&  (p1 /= pt)
+               &&  (p2 /= pt)))
+    skel
+  where
+  -- all points to which given point is connected by a segment
+  ends = S.delete pt $
+         S.fromList $
+         foldl
+           (\acc (p1, p2) -> if p1 == pt
+                                     then p2:acc
+                                     else if p2 == pt
+                                            then p1:acc
+                                            else acc)
+           []
+           skel
